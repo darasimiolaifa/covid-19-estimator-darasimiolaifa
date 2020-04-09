fcb54986bfae5750683d543d87af2ecd571b15c3
Olaifa Oluwadarasimi Ibikunle <darasimiolaifa@gmail.com>
2020-04-09 02:54:42 +0100

    feat(challenge1): Calculate infected cases and make projections
    - create the calculateInfectedcases function
    - create the calculateProjectedInfectedcases function
    - refactor the functions into a separate file and export a helper function for accessing them

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,13 @@
-const covid19ImpactEstimator = (data) => data;
+import challenge1 from './challenge1';
+
+const covid19ImpactEstimator = (data) => {
+  const outputData = challenge1(data);
+  console.log(outputData);
+
+  return {
+    data,
+    ...outputData
+  };
+};
 
 export default covid19ImpactEstimator;
