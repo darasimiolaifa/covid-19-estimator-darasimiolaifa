c8ef6d92e960e68e89f421c0c100e0610dc065ec
Olaifa Oluwadarasimi Ibikunle <darasimiolaifa@gmail.com>
2020-04-10 17:30:27 +0100

    feat(challenge3): Calculate ICU and Ventilation needs, and economic impact
    - create the calculateProjectedICUAndVentilatorCasesWithDollarLoss function
    - export a helper function for accessing it

--- src/challenge1.js
+++ src/challenge1.js
@@ -1,36 +1,59 @@
+/**
+ * This function receives the amount of reported cases, and gives both a mild
+ * and severe estimate of the assumed actual cases
+ * @param {number} reportedCases the number of days to project over for the estimation
+ * @returns {Object} An object that contains the estimated cases:
+ *    `{
+ *       mildEstimates,
+         severeEstimates
+ *     }`
+ */
 const calculateInfectedCases = (reportedCases) => ({
   mildEstimates: reportedCases * 10,
   severeEstimates: reportedCases * 50
 });
 
+/**
+ * This function receives the mild and severe estimates of current cases, and a number of days
+ * to project over, and based on this number of days projects the number of expected cases by then
+ * @param {number} daysOfProjection the number of days to project over for the estimation
+ * @param {number} mildCurrentEstimates the current mild estimates of cases
+ * @param {number} severeCurrentEstimates the current severe estimates of cases
+ * @returns {Object} An object that contains the projected estimates for the future date:
+ *    `{
+ *       mildProjection,
+         severeProjections
+ *     }`
+ */
 const calculateProjectedInfectedCases = (
-  timeToElapse,
-  periodType,
+  daysOfProjection,
   mildCurrentEstimates,
   severeCurrentEstimates
 ) => {
-  let daysOfProjection;
-
-  switch (periodType) {
-    case 'months':
-      daysOfProjection = timeToElapse * 30;
-      break;
-    case 'weeks':
-      daysOfProjection = timeToElapse * 7;
-      break;
-    default:
-      daysOfProjection = timeToElapse;
-      break;
-  }
-  const multiplier = 2 ** parseInt(daysOfProjection / 3, 10);
+  const multiplier = 2 ** Math.trunc(daysOfProjection / 3);
   return {
     mildProjections: mildCurrentEstimates * multiplier,
     severeProjections: severeCurrentEstimates * multiplier
   };
 };
 
-export default (data) => {
-  const { reportedCases, timeToElapse, periodType } = data;
+/**
+ * The entrypoint function for challenge one.
+ *
+ * @param {Object} data the raw input data as sent to the covid19 estimator function
+ * @param {number} daysOfProjection the number of days to project over for the estimation
+ * @returns {Object} An object that contains the original input, and two other property objects:
+ *    `impact: {
+ *        currentlyInfected,
+          infectionsByRequestedTime
+ *     },
+ *      severeImpact: {
+ *        currentlyInfected,
+          infectionsByRequestedTime
+ *     }`
+ */
+export default (data, daysOfProjection) => {
+  const { reportedCases } = data;
   const { mildEstimates, severeEstimates } = calculateInfectedCases(
     reportedCases
   );
@@ -39,8 +62,7 @@ export default (data) => {
     mildProjections,
     severeProjections
   } = calculateProjectedInfectedCases(
-    timeToElapse,
-    periodType,
+    daysOfProjection,
     mildEstimates,
     severeEstimates
   );
--- src/challenge2.js
+++ src/challenge2.js
@@ -1,3 +1,13 @@
+/**
+ * This function receives the already projected infections estimates, both for the mild and severe
+ * cases, and estimates what percent of that would have become critical by the given date
+ * @param {Object} impact An object containing mild projected number of cases for a given future
+ * date
+ * @param {Object} severeImpact An object containing severe projected number of cases for a given
+ * future date
+ * @returns {Object} An object that contains the original arguments with an addtional
+ * `severeCasesByRequestedTime` property
+ */
 const calculateProjectedSevereCases = (impact, severeImpact) => {
   const { infectionsByRequestedTime: mildProjections } = impact;
   const { infectionsByRequestedTime: severeProjections } = severeImpact;
@@ -8,6 +18,16 @@ const calculateProjectedSevereCases = (impact, severeImpact) => {
   return { impact, severeImpact };
 };
 
+/**
+ * This function receives the already projected severe cases estimates, both for the mild and severe
+ * cases, and estimates the amount of hospital beds that would be available by the projected date
+ * @param {Object} impact An object containing mild projected number of severe cases for a given
+ * future date
+ * @param {Object} severeImpact An object containing severe projected number of severe cases for a
+ * given future date
+ * @returns {Object} An object that contains the original arguments with an addtional
+ * `hospitalBedsByRequestedTime` property
+ */
 const calculateProjectedBedAvailability = (
   totalHospitalBeds,
   { impact, severeImpact }
@@ -15,19 +35,27 @@ const calculateProjectedBedAvailability = (
   const { severeCasesByRequestedTime: mildProjectionofCases } = impact;
   const { severeCasesByRequestedTime: severeProjectionOfCases } = severeImpact;
 
-  const currentlyAvailableBeds = Math.ceil(totalHospitalBeds * 0.35);
+  const currentlyAvailableBeds = totalHospitalBeds * 0.35;
 
   const bestCaseScenario = currentlyAvailableBeds - mildProjectionofCases;
   const worstCaseScenario = currentlyAvailableBeds - severeProjectionOfCases;
 
-  impact.hospitalBedsByRequestedTime = bestCaseScenario;
-  severeImpact.hospitalBedsByRequestedTime = worstCaseScenario;
+  impact.hospitalBedsByRequestedTime = Math.trunc(bestCaseScenario);
+  severeImpact.hospitalBedsByRequestedTime = Math.trunc(worstCaseScenario);
 
   return { impact, severeImpact };
 };
 
-export default (estimate) => {
-  const { impact, severeImpact, totalHospitalBeds } = estimate;
+/**
+ * The entrypoint function for challenge one.
+ *
+ * @param {Object} data the updated input data, with the projected infections by a future date
+ * already estimated
+ * @returns {Object} An object that contains the original properties, but extended with now
+ * projected amount of severe cases and available hospital beds at the future date
+ */
+export default (data) => {
+  const { impact, severeImpact, totalHospitalBeds } = data;
   const projectedSevereCases = calculateProjectedSevereCases(
     impact,
     severeImpact
@@ -37,8 +65,8 @@ export default (estimate) => {
     projectedSevereCases
   );
 
-  estimate.impact = projectedBedAvailabilty.impact;
-  estimate.severeImpact = projectedBedAvailabilty.severeImpact;
+  data.impact = projectedBedAvailabilty.impact;
+  data.severeImpact = projectedBedAvailabilty.severeImpact;
 
-  return estimate;
+  return data;
 };
